703b7ac36f728154e6f1c140a3bb3e3846628af9
PC <PC@192.168.1.70>
2016-10-10 18:57:55 -0500

    4.	Realizar un programa para calcular los grados °F a partir de un número de °C dado.

--- src/pp2/problema4/PP2Problema4.java
+++ src/pp2/problema4/PP2Problema4.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp2.problema4;
-
+import java.util.*;
 /**
  *
  * @author PC
@@ -15,7 +15,19 @@ public class PP2Problema4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+          convertirFahrenheit();
     }
+    public static double solicitarDatos(String d){ //Solicita datos
+        double miDato;
+        Scanner entradaEscaner = new Scanner(System.in);
+        System.out.print("Introduce el dato " + d);
+        miDato= entradaEscaner.nextDouble();
+        return miDato;
+    }
+    public static double convertirFahrenheit(){
+        double a = solicitarDatos("grados Celsius para convertir a Fahrenheit");
+        return (a*9/5)+32;
+    }
+    
     
 }
